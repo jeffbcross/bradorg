294fdb4856bff6c88dffc31ddbaf68919949b290
Jeff Cross <middlefloor@gmail.com>
2020-03-28 22:19:56 -0700

    make a component library for an internet website

---
+++ libs/components/src/lib/button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './button.scss';
+
+/* eslint-disable-next-line */
+export interface ButtonProps {}
+
+export const Button = (props: ButtonProps) => {
+  return <button>Hi</button>;
+};
+
+export default Button;
